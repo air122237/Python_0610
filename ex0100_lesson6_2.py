3df6c459ef3869de7086d16a344c6fd0aafa53c7
air122237 <77631085+air122237@users.noreply.github.com>
2023-07-08 06:47:55 +0000

    完成終極密碼

--- ex0100_lesson6_2.py
+++ ex0100_lesson6_2.py
@@ -2,11 +2,31 @@
 
 import random
 
-times=int(input(f"how many random number you need?"))
+#times=int(input(f"how many random number you need?"))
 
-for i in range(0,times):
-    i+=1
-    min=1
-    max=10
-    target= random.randint(min,max)
-    print (f"random number is {target}")
+#for i in range(0,times):
+#   i+=1
+min=1
+max=10
+count=0
+bomb= random.randint(min,max)
+#    print (f"random number is {bomb}")
+print("============終極密碼============")
+
+
+while True:
+    count+=1
+    target=int(input(f"請輸入一個[{min}~{max}間的數字"))
+    if bomb==target:
+        print(f"你輸了! 終極密碼就是{bomb}")
+        break
+    elif target<min or target>max:5
+    
+        print(f"這不是一個[{min}~{max}間的數字")
+        continue
+    elif target >bomb:
+        max=target
+        continue
+    else:
+        min=target
+        continue
