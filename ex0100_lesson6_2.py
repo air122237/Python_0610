fc5122578a15c68577029a81c8cafeb34a09fe02
air122237 <77631085+air122237@users.noreply.github.com>
2023-07-08 06:30:20 +0000

    ２３

---
+++ ex0100_lesson6_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.10.8
+
+import random
+
+times=int(input(f"幾次how many random number you need?"))
+
+for i in range(0,times):
+    i+=1
+    min=1
+    max=10
+    target= random.randint(min,max)
+    print (f"random number is {target}")
